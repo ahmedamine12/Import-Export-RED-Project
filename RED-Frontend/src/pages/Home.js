aa6e1820b1b81768562e86493ed0e6d48a09e97a
ahmedamine12 <ahmedamine.bouricha@etu.fstm.ac.ma>
2023-03-27 16:37:19 +0100

    static

--- RED-Frontend/src/pages/Home.js
+++ RED-Frontend/src/pages/Home.js
@@ -92,6 +92,7 @@ const Dashboard = () => {
                                     </li>
                                 ))}
                             </ul>
+
                         </div>
                     </div>
                 </div>
