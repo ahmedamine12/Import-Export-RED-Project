5934e2f536201b3efda5d1817f08cbd1ab2c6542
ahmedamine12 <ahmedamine.bouricha@etu.fstm.ac.ma>
2023-03-29 01:54:54 +0100

    new front

--- RED-Frontend/src/pages/Home.js
+++ RED-Frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line,BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
 import "./Home.css";
 const Dashboard = () => {
     const [redProducts, setRedProducts] = useState([]);
@@ -8,6 +9,7 @@ const Dashboard = () => {
     const loadRedProducts = async () => {
         const result = await axios.get("http://localhost:8080/redProducts")
         setRedProducts(result.data);
+        console.log(result.data);
     };
 
     useEffect(() => {
@@ -46,10 +48,26 @@ const Dashboard = () => {
         return topCountries.sort((a, b) => b.total - a.total).slice(0, 5);
     }
 
-    const barChartData = [
-        { name: 'Total Declarer', value: getTotalDeclarer() },
-        { name: 'Total Non Decharger', value: getTotalNonDecharger() },
-    ];
+    const groupedData = redProducts.reduce((acc, curr) => {
+        const { nameProject, valeur_declarer, valeur_non_decharger } = curr;
+        if (!acc[nameProject]) {
+            acc[nameProject] = {
+                nameProject,
+                valeur_declarer: 0,
+                valeur_non_decharger: 0
+            };
+        }
+        acc[nameProject].valeur_declarer += valeur_declarer;
+        acc[nameProject].valeur_non_decharger += valeur_non_decharger;
+        return acc;
+    }, {});
+
+    const lineChartData = Object.values(groupedData).map(({ nameProject, valeur_declarer, valeur_non_decharger }) => ({
+        nameProject,
+        valeur_declarer,
+        valeur_non_decharger
+    }));
+
 
     const topProductsData = getTopProducts().map(rp => {
         return { name: rp.designation, value: rp.valeur_declarer };
@@ -58,6 +76,7 @@ const Dashboard = () => {
     const topCountriesData = getTopCountries().map(tc => {
         return { name: tc.country, value: tc.total };
     });
+
 
     return (
         <div className="container-fluid">
@@ -110,35 +129,36 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-      <div className="col-md-6">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Total Declarer vs Total Non Decharger</h5>
-            <BarChart width={500} height={300} data={barChartData}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="value" fill="#8884d8" />
-            </BarChart>
-          </div>
+        <div className="col-md-6">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Total Declarer vs Total Non Decharger by Project</h5>
+                    <LineChart width={500} height={300} data={lineChartData}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="nameProject" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="valeur_declarer" stroke="red" name="Total Declarer" />
+                        <Line type="monotone" dataKey="valeur_non_decharger" stroke="blue" name="Total Non Decharger" />
+                    </LineChart>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
     <br />
     <div className="row">
       <div className="col-md-12">
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">Top 5 Products Chart</h5>
+            <h5 className="card-title">Top 5 Project</h5>
             <BarChart width={1000} height={400} data={topProductsData}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="name" />
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="value" fill="#8884d8" />
+              <Bar dataKey="value" fill="blue" />
             </BarChart>
           </div>
         </div>
